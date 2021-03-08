bed3398a96a0aaf0dfa95b1bafdd62995b227dbb
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-03-09 01:47:09 +0900

    Add RegisterPage UI

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div className={'auth-wrapper'}>
+      <div style={{ textAlign: 'center' }}>
+        <h3>Register</h3>
+      </div>
+      <form>
+        <label>Email</label>
+        <input name="email" type="email" />
+        <label>Name</label>
+        <input name="name" />
+        <label>Password</label>
+        <input name="password" type="password" />
+        <label>Password Confirm</label>
+        <input name="password_confirm" type="password" />
+
+        <input type="submit" />
+        <Link style={{ color: 'gray', textDecoration: 'none' }} to="login">
+          이미 아이디가 있다면..
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
