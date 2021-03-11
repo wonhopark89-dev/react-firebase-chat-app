6870ffa5c9c071acc525ceea4c3c2237e68d37e9
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-03-12 00:36:29 +0900

    Add user panel ui and useSelector

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { IoIosChatboxes } from 'react-icons/io'; // https://react-icons.github.io/react-icons/
+import Dropdown from 'react-bootstrap/Dropdown';
+import Image from 'react-bootstrap/Image';
+import { useSelector } from 'react-redux';
 
 function UserPanel() {
-  return <div>UserPanel</div>;
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <div>
+      {/* logo */}
+      <h3 style={{ color: 'white' }}>
+        <IoIosChatboxes />
+        {'  '} Chat App
+      </h3>
+
+      <div style={{ display: 'flex', marginBottom: '1rem' }}>
+        <Image src={user && user.photoURL} style={{ width: '30px', height: '30px', marginTop: '3px ' }} roundedCircle />
+        <Dropdown>
+          <Dropdown.Toggle style={{ backgroundColor: 'transparent', border: '0px' }} id="dropdown-basic">
+            {user.displayName}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">로그아웃</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
 
 export default UserPanel;
