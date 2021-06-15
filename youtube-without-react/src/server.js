d6e18b4a30512ae2119dee22d4db9e54829fd7dc
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-06-15 23:43:19 +0900

    Add package, morgan for log

--- youtube-without-react/src/server.js
+++ youtube-without-react/src/server.js
@@ -1,27 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 const app = express();
+const logger = morgan('combined');
 
 // request
 // response
 // next
 // -> express 에서 자동으로 해당 이벤트값을 넘여줌 ( req, res, next )
 // MVC, controller == middleware
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === '/protected') {
-    return res.send('<h1>Not Allowed</h1>');
-  }
-  console.log('Allowed...');
-  next();
-};
 
 const handleHome = (req, res) => {
   return res.send('I like middleware');
@@ -31,8 +19,8 @@ const handleProtected = (req, res) => {
   return res.send('Welcome to private lounge');
 };
 
-app.use(logger); // 모든 route 에서 use 를 사용 ( as global middleware ) 순서 중요
-app.use(privateMiddleware);
+// 모든 route 에서 use 를 사용 ( as global middleware ) 순서 중요
+app.use(logger);
 app.get('/', handleHome); // get request to root path
 app.get('/protected', handleProtected);
 
